7f2e3c7642690369586f7c24fc4c51f720caf456
Patti85 <pbeier24@googlemail.com>
2020-08-28 19:27:20 +0200

    firstFunc

--- app/src/main/java/com/pheimfarth/learningrxjava/MainActivity.java
+++ app/src/main/java/com/pheimfarth/learningrxjava/MainActivity.java
@@ -1,8 +1,14 @@
 package com.pheimfarth.learningrxjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        simpleFunctionWithObserverAndLogs();
+
+    }
+
+    private void simpleFunctionWithObserverAndLogs(){
+        Observable<String> membersObservable =
+                Observable.just("Steven", "Jane", "Phillip");
+
+        Observer<String> membersObserver = getMembersObserver();
+
+        membersObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(membersObserver);
+    }
+
+    private Observer<String> getMembersObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d("Meow", "Start!");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d("Meow", "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e("Meow", "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d("Meow", "Completed!");
+            }
+        };
     }
 }
